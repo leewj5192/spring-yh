2d53d0e95450c172ef0f1973abeadf1994d9b9f9
wj <leewj5192@gmail.com>
2023-11-06 19:26:29 +0900

    11/6

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,76 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @Test
+    void 회원가입() {
+        //given 주어졌을때
+        Member member = new Member();
+        member.setName("hello");
+
+
+        //when 실행했을떄
+        Long saveId = memberService.join(member);
+
+        //then 결과
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        // FIX
+        memberService.join(member1);
+        // IllegalStateException e = assertThrows(IllegalStateException.class, ()->memberService.join(member2));
+        // assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+
+/*
+        try {
+            memberService.join(member2);
+            fail();
+        }catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+
+*/
+        // then
+    }
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
