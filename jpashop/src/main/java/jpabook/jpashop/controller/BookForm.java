da70a3c4744948028ccd31545eb1dfb7839f5dc2
wj <leewj5192@gmail.com>
2023-11-13 18:47:44 +0900

    11/13

---
+++ jpashop/src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.controller;
+
+public class BookForm {
+
+    private  Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String author;
+    private String isbn;
+}
--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,16 +10,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy =  InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter @Setter
 public abstract class Item {
-    @Id @GeneratedValue
+
+    @Id
+    @GeneratedValue
     @Column(name = "item_id")
     private Long id;
+
     private String name;
     private int price;
     private int stockQuantity;
+
     @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<Category>();
+    private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    /**
+     * 스톡증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 스톡감소
+     */
+    public  void removeStock(int quantity) {
+        int restStock  = this.stockQuantity - quantity;
+        if(restStock <0) {
+            throw  new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
